39b1ed273831afd1e83c097df9d5770f803d6b24
MadiyarGGGGG <madiyar.tolegen.0@mail.ru>
2025-07-28 12:17:04 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Api from './Ai/Api'
-
-// import ParkingMap from './ParkingMap'
-
-// import App from './Progect1/App'
-// import './Progect1/Style.css'
-
-import MovieFinder from "./Progect1/MovieFinder";
-import DI from "./Progect1/DI";
+import Api from './Ai/Api'
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-<DI/>
+<Api/>
   </StrictMode>
 
  
